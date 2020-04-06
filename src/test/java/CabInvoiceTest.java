cb13c03624001993461e9f19cb5e176dbb8ff288
rahulgidde <rahulgidde1212@gmail.com>
2020-04-06 11:04:30 +0530

    Added:Test Case For Minimum Fare

--- src/test/java/CabInvoiceTest.java
+++ src/test/java/CabInvoiceTest.java
@@ -10,4 +10,10 @@ public class CabInvoiceTest {
         double result = cabInvoice.getTotalFare(5, 10);
         Assert.assertEquals(result, 60, 0);
     }
+
+    @Test
+    public void givenDistanceAndTime_InvoiceGeneratorCalculateFare_ShouldReturnMinimumFare() {
+        double result = cabInvoice.getTotalFare(0.1, 4);
+        Assert.assertEquals(5, result, 0);
+    }
 }
